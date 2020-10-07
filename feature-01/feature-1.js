e757d6b4a8088603e1d70d7141aba99a4d33b70c
Giovanna Santos <giovannadossantos933@gmail.com>
2020-10-07 01:16:03 -0300

    :Package: Programa que le duas idades e diz qual é mais velho

---
+++ feature-01/feature-1.js
@@ -0,0 +1,21 @@
+var idadePrimeiraPessoa = 23.000
+var idadeSegundaPessoa = 20
+
+if (idadePrimeiraPessoa > idadeSegundaPessoa) {
+    console.log(`A primeira pessoa é mais velha(o) que a segunda pessoa \nidade: ${idadePrimeiraPessoa}`)
+    
+}
+
+else if(idadeSegundaPessoa > idadePrimeiraPessoa){
+    console.log(`A segunda pessoa é mais velha(o) que a pegunda pessoa \nidade: ${idadeSegundaPessoa}`)
+}
+
+else if ( idadeSegundaPessoa == idadePrimeiraPessoa){
+    console.log(`As duas pessoas tem a mesma idade \nPrimeira pessoa ${idadePrimeiraPessoa}, Segunda pessoa ${idadeSegundaPessoa}`)
+
+}
+
+else{
+
+    console.log(`X erro interno!!!!!!!!!!!!!!!!!!!!!!!!!!! X`)
+}
