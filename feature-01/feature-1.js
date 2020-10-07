85285215b71ad23fe5129df264bfa8cda77eb842
Giovanna Santos <giovannadossantos933@gmail.com>
2020-10-07 01:39:23 -0300

    :package: Programa que le sua idade e diz se já pode ou não dirijir

--- feature-01/feature-1.js
+++ feature-01/feature-1.js
@@ -19,3 +19,5 @@ else{
 
     console.log(`X erro interno!!!!!!!!!!!!!!!!!!!!!!!!!!! X`)
 }
+
+
---
+++ feature-01/feature-2.js
@@ -0,0 +1,31 @@
+var idadePrimeira = 17
+var idadeSegunda = 17
+
+
+if( idadePrimeira >= 18 && idadeSegunda < 18)
+{
+    console.log(`Você te mais de 18 anos e já pode dirigir, \nPessoa mais velha com a idade de ${idadePrimeira}, Segunda pessoa ainda é de menor e legalmente não pode dirigir, ${idadeSegunda}`)
+}
+
+
+else if(idadeSegunda >=18 &&  idadePrimeira <18){
+    console.log(`Você te mais de 18 anos e já pode dirigir, \nPessoa mais velha com a idade de ${idadeSegunda}, Segunda pessoa ainda é de menor e legalmente não pode dirigir, ${idadePrimeira}`)
+}
+
+else if (idadePrimeira >= 18 && idadeSegunda >= 18){
+
+    console.log(`As duas pessoas estão aptas para Dirigir, Dirija com cuidado <3`)
+
+}
+
+else if ( idadePrimeira <18 && idadeSegunda <18){
+
+    console.log(`As duas pessoas não estão aptas a dirigir pois são de menor idade :c`)
+
+
+}
+
+else {
+    console.log(`Erro interno!!!!!!!!!!!!!!!!!!!`)
+}
+
