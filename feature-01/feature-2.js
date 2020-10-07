63bd19b2f29aefdd896bdb6cbc1559ace92c29bc
migueldossantos011@gmail.com <migueldossantos011@gmail.com>
2020-10-07 01:59:19 -0300

    :package: BUG Code Review FEATURE-02 :man_technologist:

--- feature-01/feature-2.js
+++ feature-01/feature-2.js
@@ -1,28 +1,21 @@
-var idadePrimeira = 17
-var idadeSegunda = 17
+var idadePrimeiraPessoa = 18
+var idadeSegundaPessoa = 7
 
 
-if( idadePrimeira >= 18 && idadeSegunda < 18)
-{
-    console.log(`Você te mais de 18 anos e já pode dirigir, \nPessoa mais velha com a idade de ${idadePrimeira}, Segunda pessoa ainda é de menor e legalmente não pode dirigir, ${idadeSegunda}`)
+if( idadePrimeiraPessoa >= 18 && idadeSegundaPessoa < 18){
+    console.log(`Você te mais de 18 anos e já pode dirigir, \nPessoa mais velha com a idade de ${idadePrimeiraPessoa}, Segunda pessoa ainda é de menor e legalmente não pode dirigir, ${idadeSegundaPessoa}`)
 }
 
-
-else if(idadeSegunda >=18 &&  idadePrimeira <18){
-    console.log(`Você te mais de 18 anos e já pode dirigir, \nPessoa mais velha com a idade de ${idadeSegunda}, Segunda pessoa ainda é de menor e legalmente não pode dirigir, ${idadePrimeira}`)
+else if(idadeSegundaPessoa >= 18 &&  idadePrimeiraPessoa < 18){
+    console.log(`Você te mais de 18 anos e já pode dirigir, \nPessoa mais velha com a idade de ${idadeSegundaPessoa}, Segunda pessoa ainda é de menor e legalmente não pode dirigir, ${idadePrimeiraPessoa}`)
 }
 
-else if (idadePrimeira >= 18 && idadeSegunda >= 18){
-
+else if (idadePrimeiraPessoa >= 18 && idadeSegundaPessoa >= 18){
     console.log(`As duas pessoas estão aptas para Dirigir, Dirija com cuidado <3`)
-
 }
 
-else if ( idadePrimeira <18 && idadeSegunda <18){
-
+else if ( idadePrimeiraPessoa < 18 && idadeSegundaPessoa < 18){
     console.log(`As duas pessoas não estão aptas a dirigir pois são de menor idade :c`)
-
-
 }
 
 else {
@@ -30,4 +23,4 @@ else {
 }
 
 
-30 min
+// 30 min
