5e636353f07118b625e9db74a39dce3771d4d58b
Giovanna Santos <giovannadossantos933@gmail.com>
2020-10-18 17:07:46 -0300

    :package: mudule da feature-9 :man_technologist:

---
+++ feature-01/feature-10.js
@@ -0,0 +1,2 @@
+const mult = require("./feature-9")
+console.log(mult (2, 2))
---
+++ feature-01/feature-9.js
@@ -0,0 +1,5 @@
+ function Multiplicacao (num1 = 0, num2 = 0) {
+    return num1 * num2   
+ }
+
+ module.exports = Multiplicacao
