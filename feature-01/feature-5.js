19dbaca859e3dc5f20b44f046615301fcb0dbf65
Giovanna Santos <giovannadossantos933@gmail.com>
2020-10-18 02:19:30 -0300

    :package: criando module.exports da função subtração :man_technologist:

--- feature-01/feature-5.js
+++ feature-01/feature-5.js
@@ -1,9 +1,9 @@
 function Subtracao(num1 = 0 , num2 = 0) {
     return num1 - num2
-    
 }
 
-let subtracao = Subtracao (6,9)
-console.log(`O resultado da Subtração é ${subtracao}`)
+ module.exports = Subtracao
+
+
 
 //15 min
---
+++ feature-01/feature-6.js
@@ -0,0 +1,5 @@
+const subtracao = require("./feature-5")
+console.log(subtracao(7, 1))
+
+// 20 min
+
